362bdd4ffade10ab9f177ca260f493a022b81631
PALAKEERTHI NAVEEN <naveenpalakeerthi2003@gmail.com>
2024-12-07 22:33:13 +0530

    Add files via upload

---
+++ Config.java
@@ -0,0 +1,7 @@
+package config;
+
+public class Config {
+    public static final String DB_URL = "jdbc:mysql://@localhost:3306/miniproject";
+    public static final String USER = "root";
+    public static final String PASSWORD = "sree";
+}
